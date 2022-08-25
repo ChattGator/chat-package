daf1cd5f4e34081cfa7d328d35647e94bdaa386b
Barun Debnath <barundebnath91@gmail.com>
2022-08-25 13:46:44 +0530

    bundling error solved,but integration error - webpack arised

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,21 @@
-import { Button } from "chat-ui-kit";
+import {
+  BioSectionContainer,
+  Button,
+  ChatBody,
+  Container,
+  RecentChatBody,
+} from "chat-ui-kit";
 import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
-  return <Button label="Hello" classes="buttonClass" />;
+  return (
+    <Container>
+      <RecentChatBody />
+      <ChatBody />
+      <BioSectionContainer />
+    </Container>
+  );
 }
 
 export default App;
